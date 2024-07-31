a94b93f44d32874f97d839ac055bebb071489d14
unknown <laarychambas@gmail.com>
2024-07-31 16:26:37 -0400

    uncommented

--- practical-loops.js
+++ practical-loops.js
@@ -1,14 +1,14 @@
-// for (let i = 1; i <= 100; i++) {
-//   if (i % 3 === 0 && i % 5 === 0) {
-//     console.log("Fizz Buzz");
-//   } else if (i % 5 === 0) {
-//     console.log("Buzz");
-//   } else if ( i % 3 === 0  ) {
-//     console.log("Fizz")
-//   } else {
-//     console.log(i);
-//   }
-// }
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Fizz Buzz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if ( i % 3 === 0  ) {
+    console.log("Fizz")
+  } else {
+    console.log(i);
+  }
+}
 
 let n = 4
 //check if n is Prime
